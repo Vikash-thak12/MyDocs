0fbbb60b34b2556267483db9f167e987cab71e72
Vikash Thakur <thakvikash622@gmail.com>
2024-08-31 16:07:14 +0530

    Removed the necessary data

--- lib/liveblocks.ts
+++ lib/liveblocks.ts
@@ -1,5 +1,5 @@
 import { Liveblocks } from "@liveblocks/node";
 
 export const liveblocks = new Liveblocks({
-    secret: "sk_dev_8kUrAUXBnBk5NvIRfv3XQWJea_jyD-l4VakLEtR7Zx1mw2J80ZDB2O-_RMrSywpy",
+    secret: "",
   });
